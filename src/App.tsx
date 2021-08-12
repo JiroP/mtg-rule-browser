3218ef41780d51f69756cad261181176f116afca
Jiro Pitkänen <jiro.pitkanen@flovv.io>
2021-08-12 19:08:03 +0300

    Request to cors proxy to get mtg rules
    Installed axios to process request. Created a small service to proxy request
    cause request target does not set CORS.
    
    Added index.tsx back, it was deleted accidentally on previous commit.
    
    useEffect hook to fetch rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
-import React, { ReactElement } from 'react';
-import './App.css';
+import React, { ReactElement, useEffect } from 'react';
+import axios from 'axios';
+// import './App.css';
 
-const App: React.FC = (): ReactElement => <div>Hello world</div>;
+const App: React.FC = (): ReactElement => {
+  useEffect(() => {
+    const fetchRules: () => Promise<void> = async () => {
+      const resp = await axios.get(
+        'http://localhost:3001',
+      );
+      console.log(resp);
+    };
+
+    fetchRules();
+  }, []);
+
+  return <div>Hello world</div>;
+};
 
 export default App;
